692f001b0ab6b8c3e0c78caf4e8f3ab6460452cf
Shravya Donthisaram <61753303+dshravya1@users.noreply.github.com>
2020-12-01 20:00:22 -0500

    Update Dijkstra.go

--- Dijkstra.go
+++ Dijkstra.go
@@ -7,35 +7,48 @@ import (
 	"math"
 )
 
+//number of vertices in the graph
 const (
 	V int = 9
 )
 
 // Function to find the find the vertex with minimum distance value,
-//from the set of vertices which are not yer included in tree of shortest path
-
-func minimum_distance(distance [V]int, vertices [V]bool) int {
+//from the set of vertices which are not yer included in tree of shortest pathxw
+func FindMinimumDistance(distance [V]int, vertices [V]bool) int {
 
 	// Initialize min value
 	var min int = math.MaxInt64
-	var min_index int = -1
+	var minIndex int = -1
 	for i := 0; i < V; i++ {
 		if vertices[i] == false && distance[i] <= min {
 			min = distance[i]
-			min_index = i
+			minIndex = i
 		}
 
 	}
-	return min_index
+	return minIndex
 }
 
 //  Function to print the final result
 
-func final_result(distance [V]int, n int) {
+func DisplayResult(distance [V]int, n int, path [V]int) {
 	fmt.Println(" Distance of vertex from the source:")
 	for i := 0; i < V; i++ {
-		fmt.Println("Vertex ", i, "is at a distance of ", distance[i])
+		fmt.Println("\n\nVertex ", i, "is at a distance of ", distance[i], "\nPath is")
+		Printpath(path, i)
 	}
+
+}
+
+func Printpath(path [V]int, n int) {
+	// Base case if n is source
+	if path[n] == -1 {
+		fmt.Printf("%d %s", n, " ")
+		return
+	}
+	Printpath(path, path[n])
+	fmt.Printf("%d %s", n, " ")
+
 }
 
 // Function for the implementation of Dijkstra's single source shortest path
@@ -44,9 +57,11 @@ func final_result(distance [V]int, n int) {
 func dijkstra(graph [][]int, source int) {
 	var distance [V]int  // Array for shortest distance from source to i
 	var vertices [V]bool // Array for tracking visited vertices
+	var path [V]int      // Array for storing vertices following the shortest path
 
-	// Initializing all the distances to the vertices as INFINITE and Vertices[] as false
+	// Initializing path to source as -1, all the distances to the vertices as INFINITE and Vertices[] as false
 	for i := 0; i < V; i++ {
+		path[0] = -1
 		distance[i] = math.MaxInt64
 		vertices[i] = false
 	}
@@ -59,7 +74,7 @@ func dijkstra(graph [][]int, source int) {
 
 		// Considering the minimum distance vertex from the set of vertices not yet visited.
 		// u is always the source in first iteration
-		var u int = minimum_distance(distance, vertices)
+		var u int = FindMinimumDistance(distance, vertices)
 
 		// Updating the visited vertex as processed
 		vertices[u] = true
@@ -72,25 +87,27 @@ func dijkstra(graph [][]int, source int) {
 			if !vertices[r] && graph[u][r] != 0 &&
 				distance[u] != math.MaxInt64 && distance[u]+graph[u][r] < distance[r] {
 				distance[r] = distance[u] + graph[u][r]
+				path[r] = u
 			}
+
 		}
 	}
 	// To print the distance array representing shortest paths from source to other vertices
-	final_result(distance, V)
+	DisplayResult(distance, V, path)
 }
 
 // creating the example graph using adjacent matrix representation
 
 func main() {
-	var graph = [][]int{
-		{0, 4, 0, 0, 0, 0, 0, 8, 0},
-		{4, 0, 8, 0, 0, 0, 0, 11, 0},
-		{0, 8, 0, 7, 0, 4, 0, 0, 2},
-		{0, 0, 7, 0, 9, 14, 0, 0, 0},
-		{0, 0, 0, 9, 0, 10, 0, 0, 0},
-		{0, 0, 4, 14, 10, 0, 2, 0, 0},
-		{0, 0, 0, 0, 0, 2, 0, 1, 6},
-		{8, 11, 0, 0, 0, 0, 1, 0, 7},
-		{0, 0, 2, 0, 0, 0, 6, 7, 0}}
+	var graph = [][]int{{0, 4, 0, 0, 0, 0, 0, 8, 0},
+		{3, 0, 7, 0, 0, 0, 0, 10, 0},
+		{0, 7, 0, 6, 0, 3, 0, 0, 1},
+		{0, 0, 6, 0, 8, 13, 0, 0, 0},
+		{0, 0, 0, 8, 0, 9, 0, 0, 0},
+		{0, 0, 3, 0, 10, 0, 1, 0, 0},
+		{0, 0, 0, 13, 0, 1, 0, 2, 5},
+		{8, 10, 0, 0, 0, 0, 2, 0, 6},
+		{0, 0, 1, 0, 0, 0, 5, 6, 0}}
+
 	dijkstra(graph, 0)
 }
